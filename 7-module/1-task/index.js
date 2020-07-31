a59a2e980beed72cae15c2eb9b352cf64420b0d0
Julia Andreeva <julandre@protonmail.com>
2020-07-31 19:46:32 +0300

    promise

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -5,14 +5,6 @@
  */
 
 
-/*
-function promiseClick(button) {
- return new Promise(function(resolve, reject) {
-   button.addEventListener('click', event => resolve(event));
- 
- });
-}
-*/
 
 function promiseClick(button) {
  return new Promise(function(resolve, reject) {
